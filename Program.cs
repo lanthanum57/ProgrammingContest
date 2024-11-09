7203396c374940a2cfb44bf1dee2336f3b6e0fad
yuta maeda <yu2ww88@gmail.com>
2024-11-10 00:46:04 +0900

    Chapter02追加

--- Program.cs
+++ Program.cs
@@ -4,23 +4,40 @@ class Program
     static void Main(string[] args)
     {
         string[] input = Console.ReadLine().Split(" ");
-        int n = int.Parse(input[0]); // 使う整数：1～3000
-        int k = int.Parse(input[1]); // 合計：3～9000
+        int N = int.Parse(input[0]);
+        int Q = int.Parse(input[1]);
 
-        int count = 0;
+        string[] A = Console.ReadLine().Split(" ");
 
-        for (int i = 1; i <= n; i++)
+        // 累積和を作成
+        int[] SumA = new int[N];
+        int sum = 0;
+        for (int i = 0; i < N; i++)
         {
-            for (int j = 1; j <= n; j++)
-            {
-                int l = k - i - j;
-                if (l >= 1 && l <= n)
-                {
-                    count++;
-                }
-            }
+            SumA[i] = sum + int.Parse(A[i]);
+            sum += int.Parse(A[i]);
         }
 
-        Console.WriteLine(count);
+        int[] result = new int[Q];
+        for (int i = 0; i < Q; i++)
+        {
+            input = Console.ReadLine().Split(" ");
+            int begin = int.Parse(input[0]);
+            int end = int.Parse(input[1]);
+
+            if (begin >= 2)
+            {
+                result[i] = SumA[end - 1] - SumA[begin - 2];
+            }
+            else
+            {
+                result[i] = SumA[end - 1];
+            }
+        }
+        
+        foreach (var r in result)
+        {
+            Console.WriteLine(r);
+        }
     }
 }
---
+++ kyopgm/Chapter02.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingContest.kyopgm
+{
+    internal class Chapter02
+    {
+        public void A06()
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            int N = int.Parse(input[0]);
+            int Q = int.Parse(input[1]);
+
+            string[] A = Console.ReadLine().Split(" ");
+
+            // 累積和を作成
+            int[] SumA = new int[N];
+            int sum = 0;
+            for (int i = 0; i < N; i++)
+            {
+                SumA[i] = sum + int.Parse(A[i]);
+                sum += int.Parse(A[i]);
+            }
+
+            int[] result = new int[Q];
+            for (int i = 0; i < Q; i++)
+            {
+                input = Console.ReadLine().Split(" ");
+                int begin = int.Parse(input[0]);
+                int end = int.Parse(input[1]);
+
+                if (begin >= 2)
+                {
+                    result[i] = SumA[end - 1] - SumA[begin - 2];
+                }
+                else
+                {
+                    result[i] = SumA[end - 1];
+                }
+            }
+
+            foreach (var r in result)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
