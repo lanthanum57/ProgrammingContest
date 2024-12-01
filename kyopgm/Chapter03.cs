d641fefe9e2400607e36ebea7c5b82c36ad2e0b9
yuta maeda <yu2ww88@gmail.com>
2024-12-01 11:13:26 +0900

    A11追加。 繰り返し使用できるアルゴリズムは共通メソッド化。

---
+++ kyopgm/Chapter03.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingContest.kyopgm
+{
+    internal class Chapter03
+    {
+        // 二分探索
+        private void A11()
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            int N = int.Parse(input[0]);
+            int X = int.Parse(input[1]);
+
+            string[] AN = Console.ReadLine().Split(" ");
+
+            int beginIndex = 1;
+            int endIndex = N;
+            while (beginIndex < endIndex)
+            {
+                int centerIndex = (beginIndex + endIndex) / 2;
+
+                int centerValue = int.Parse(AN[centerIndex - 1]);
+
+                if (X == centerValue)
+                {
+                    beginIndex = centerIndex;
+                    break;
+                }
+                else if (X > centerValue)
+                {
+                    beginIndex = centerIndex + 1;
+                }
+                else
+                {
+                    endIndex = centerIndex - 1;
+                }
+            }
+
+            Console.WriteLine(beginIndex);
+        }
+    }
+}
---
+++ lib/CommonArgorithm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingContest.lib
+{
+    internal class CommonArgorithm
+    {
+        /// <summary>
+        /// 与えられた配列(targetList)のどこに指定の値(targetValue)が存在するか
+        /// 二分探索手法で検索して、一致するindexを返します。
+        /// 計算量はlogNとなります。
+        /// </summary>
+        /// <param name="targetList">検索対象の配列</param>
+        /// <param name="targetValue">検索対象の値</param>
+        /// <returns></returns>
+        public static int BinarySearch(string[] targetList, int targetValue)
+        {
+            int beginIndex = 1;
+            int endIndex = targetList.Length;
+            while (beginIndex < endIndex)
+            {
+                int centerIndex = (beginIndex + endIndex) / 2;
+
+                int centerValue = int.Parse(targetList[centerIndex - 1]);
+
+                if (targetValue == centerValue)
+                {
+                    beginIndex = centerIndex;
+                    break;
+                }
+                else if (targetValue > centerValue)
+                {
+                    beginIndex = centerIndex + 1;
+                }
+                else
+                {
+                    endIndex = centerIndex - 1;
+                }
+            }
+
+            return beginIndex;
+        }
+    }
+}
